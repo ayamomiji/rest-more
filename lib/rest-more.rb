6450a36dce6efa9c003f91140a6a3aae1bff1782
ayaya <ayaya@ayaya.tw>
2011-11-23 02:04:54 +0800

    autoload GooglePlus

--- lib/rest-more.rb
+++ lib/rest-more.rb
@@ -2,13 +2,14 @@
 require 'rest-core'
 
 module RestCore
-  autoload :Config  , 'rest-core/util/config'
+  autoload :Config     , 'rest-core/util/config'
 
-  autoload :Bing    , 'rest-core/client/bing'
-  autoload :Facebook, 'rest-core/client/facebook'
-  autoload :Flurry  , 'rest-core/client/flurry'
-  autoload :Github  , 'rest-core/client/github'
-  autoload :Linkedin, 'rest-core/client/linkedin'
-  autoload :Mixi    , 'rest-core/client/mixi'
-  autoload :Twitter , 'rest-core/client/twitter'
+  autoload :Bing       , 'rest-core/client/bing'
+  autoload :Facebook   , 'rest-core/client/facebook'
+  autoload :Flurry     , 'rest-core/client/flurry'
+  autoload :Github     , 'rest-core/client/github'
+  autoload :Linkedin   , 'rest-core/client/linkedin'
+  autoload :Mixi       , 'rest-core/client/mixi'
+  autoload :Twitter    , 'rest-core/client/twitter'
+  autoload :GooglePlus , 'rest-core/client/google_plus'
 end
